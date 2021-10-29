1ad38705fda6614718b627ed161328583bc01e4c
Bruno Lessa <brunolessa@live.com>
2021-10-29 01:34:17 -0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExemplosValueTypesEReferencesTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // int, float, double, bool, char...
+            int x = 10;
+            Console.WriteLine(x);
+            SumTwo(x);
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+
+        static void SumTwo(int number)
+        {
+            number += 2;
+        }
+    }
+}
